903e0bcf7bd06bfa24cc52889e34350e21619284
Aleksey <rusal@bk.ru>
2023-01-20 19:59:44 +0300

    first commit

---
+++ createJson.py
@@ -0,0 +1,15 @@
+﻿import json
+data = []
+quest1 = {}
+quest1["quest"] = "Что такое ёжик?"
+Answers = [
+    ["птица", 0],
+    ["человек", 0],
+    ["млекопитающее", 1]
+]
+quest1["answers"] = Answers
+data.append(quest1)
+jsonstring  = json.dumps(data,ensure_ascii=False)
+print(jsonstring)
+with open('data.json', 'w', encoding='utf-8') as f:
+    f.write(jsonstring)
---
+++ tester.py
@@ -0,0 +1,22 @@
+﻿import json
+content = ""
+with open('data.json', 'r', encoding='utf-8') as f:
+    content = f.read()
+print(content)
+data = json.loads(content)
+quest = data[0]
+print(quest["quest"])
+answers = quest["answers"]
+num = 0
+for answer in answers:
+    num += 1
+    print(f"{num} {answer[0]}")
+print("Введите номер верного ответа:")
+answer_num = int(input())-1
+
+if answer_num < 1 and answer_num > len(answers):
+    print("Не правильно")
+elif answers[answer_num][1] == 1:
+    print("Правильно")
+else:
+    print("Не правильно")

229dd8a811eb0bae571264977b66b24becb442ad
Aleksey <rusal@bk.ru>
2023-01-27 17:28:03 +0300

    fix json

--- data_creator.py
+++ data_creator.py
@@ -1,33 +1,47 @@
-﻿categories = []
-item_cat1={}
-item_cat1["id"]= 1
-item_cat1["name"]= "Хобби"
-item_cat1["desc"]= "Ваши увлечения"
+﻿import json
+categories = []
+item_cat1 = {}
+item_cat1["id"] = 1
+item_cat1["name"] = "Хобби"
+item_cat1["desc"] = "Ваши увлечения"
+sub_categories = []
+item_sub_cat1 = {}
+item_sub_cat1["id"] = 1
+item_sub_cat1["name"] = "Коллекционирование марок"
+item_sub_cat1["desc"] = "История и оценки марок"
+# item_sub_cat1["image"] = "История и оценки марок"
+
+sub_categories.append(item_sub_cat1)
+item_sub_cat2 = {}
+item_sub_cat2["id"] = 2
+item_sub_cat2["name"] = "Вышивание"
+item_sub_cat2["desc"] = "Примеры ручных работ"
+sub_categories.append(item_sub_cat2)
+item_cat1["categories"] = sub_categories
 categories.append(item_cat1)
-item_cat2={}
-item_cat2["id"]= 2
-item_cat2["name"]= "Домашние животные"
-item_cat2["desc"]= "Увлечения с вашими любимцами"
+
+item_cat2 = {}
+item_cat2["id"] = 2
+item_cat2["name"] = "Домашние животные"
+item_cat2["desc"] = "Увлечения с вашими любимцами"
+sub_categories = []
+item_sub_cat1 = {}
+item_sub_cat1["id"] = 1
+item_sub_cat1["name"] = "Кошки"
+item_sub_cat1["desc"] = "Обсуждают своих домашних питомцев"
+sub_categories.append(item_sub_cat1)
+item_sub_cat2 = {}
+item_sub_cat2["id"] = 2
+item_sub_cat2["name"] = "Собаки"
+item_sub_cat2["desc"] = "Обсуждают своих домашних питомцев"
+sub_categories.append(item_sub_cat2)
+item_sub_cat3 = {}
+item_sub_cat3["id"] = 2
+item_sub_cat3["name"] = "Хомячки"
+item_sub_cat3["desc"] = "Обсуждают своих домашних питомцев"
+sub_categories.append(item_sub_cat3)
+item_cat2["categories"] = sub_categories
 categories.append(item_cat2)
-import json
 jsonstring = json.dumps(categories, ensure_ascii=False)
 with open('categories.json', 'w+', encoding='utf-8') as f:
-    f.write(jsonstring)
-    
-sub_categories = []
-item_sub_cat1={}
-item_sub_cat1["id"]= 1
-item_sub_cat1["parent_id"]= 1
-item_sub_cat1["name"]= "Коллекционирование марок"
-item_sub_cat1["desc"]= "История и оценки марок"
-sub_categories.append(item_sub_cat1)
-item_sub_cat2={}
-item_sub_cat2["id"]= 2
-item_sub_cat2["parent_id"]= 1
-item_sub_cat2["name"]= "Философия"
-item_sub_cat2["desc"]= "Обсуждают материалы по философии"
-sub_categories.append(item_sub_cat2)
-import json
-jsonstring = json.dumps(sub_categories, ensure_ascii=False)
-with open('sub_categories.json', 'w+', encoding='utf-8') as f:
     f.write(jsonstring)
